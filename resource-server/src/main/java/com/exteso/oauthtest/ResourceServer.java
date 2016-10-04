56cdcd5d06fbc4415d8efd91b1e2321233f815ff
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-10-04 17:32:15 +0200

    cleanup

--- resource-server/src/main/java/com/exteso/oauthtest/ResourceServer.java
+++ resource-server/src/main/java/com/exteso/oauthtest/ResourceServer.java
@@ -3,19 +3,15 @@ package com.exteso.oauthtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 @SpringBootApplication
@@ -34,9 +30,7 @@ public class ResourceServer {
     @PreAuthorize("#oauth2.hasScope('resource-server-read')")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Map<String, Object> home() {
-        Map<String, Object> m = new HashMap<>();
-        m.put("message", message);
-        return m;
+        return Collections.singletonMap("message", message);
     }
 
     @PreAuthorize("#oauth2.hasScope('resource-server-write')")
