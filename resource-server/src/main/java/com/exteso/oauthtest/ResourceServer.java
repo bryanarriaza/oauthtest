39634a44d94651d36b1b119f914119f42240c6c8
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-10-04 11:23:54 +0200

    fix configuration in resource server

--- resource-server/src/main/java/com/exteso/oauthtest/ResourceServer.java
+++ resource-server/src/main/java/com/exteso/oauthtest/ResourceServer.java
@@ -2,6 +2,7 @@ package com.exteso.oauthtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Map;
 @SpringBootApplication
 @RestController
 @EnableResourceServer
+@EnableOAuth2Sso
 public class ResourceServer {
 
     public static void main(String[] args) {
