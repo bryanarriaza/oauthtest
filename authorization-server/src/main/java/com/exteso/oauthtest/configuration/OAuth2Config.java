27edec9e353632f15e178f0c6a89922af0091444
yankedev <yankedev@exteso.com>
2016-10-06 00:33:06 +0200

    change AS context path to /auth

--- authorization-server/src/main/java/com/exteso/oauthtest/configuration/OAuth2Config.java
+++ authorization-server/src/main/java/com/exteso/oauthtest/configuration/OAuth2Config.java
@@ -1,4 +1,4 @@
-package com.exteso.oauthtest.config;
+package com.exteso.oauthtest.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
